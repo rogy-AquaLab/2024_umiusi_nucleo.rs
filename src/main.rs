01433f4077524aea0745ae7d2a72346fd4bb66c4
H1rono <hronok66@gmail.com>
2024-09-03 00:30:15 +0900

    :wrench: Configure `lints.clippy`, fix errors

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,3 @@
-//! Uses the StatefulOutputPin embedded_hal trait to toggle the pin
-//! On the stm32 discovery board this is the "south" led
-//! Target board: STM32F3DISCOVERY
-
 #![deny(unsafe_code)]
 #![no_main]
 #![no_std]
