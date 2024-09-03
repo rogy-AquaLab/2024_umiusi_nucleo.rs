6f0f2a11f101fdec03db4b4a4abb28dc6cbdb736
H1rono <hronok66@gmail.com>
2024-09-03 16:52:45 +0900

    :sparkles: Inject debug code

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,7 @@ use hal::rcc::RccExt;
 
 #[entry]
 fn main() -> ! {
+    defmt::debug!("entry");
     let dp = pac::Peripherals::take().unwrap();
 
     let mut rcc = dp.RCC.constrain();
@@ -23,18 +24,22 @@ fn main() -> ! {
     let mut led = gpioa
         .pa1
         .into_push_pull_output(&mut gpioa.moder, &mut gpioa.otyper);
+    defmt::debug!("done initializing");
 
     led.set_low().unwrap();
 
     loop {
+        defmt::debug!("toggle");
         led.toggle().unwrap();
         cortex_m::asm::delay(8_000_000);
         // Toggle by hand.
         // Uses `StatefulOutputPin` instead of `ToggleableOutputPin`.
         // Logically it is the same.
         if led.is_set_low().unwrap() {
+            defmt::debug!("high");
             led.set_high().unwrap();
         } else {
+            defmt::debug!("low");
             led.set_low().unwrap();
         }
         cortex_m::asm::delay(8_000_000);
